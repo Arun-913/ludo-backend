d19acab71ef3e50b6a21fc9b617930fba17c5d84
Arun-913 <138594295+Arun-913@users.noreply.github.com>
2024-05-09 17:56:21 +0530

    Delete backend1/dist directory

--- backend1/dist/message.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OPENED = exports.NOT_OPENED = exports.WON = exports.GAME_OVER = void 0;
-exports.GAME_OVER = 'game_over';
-exports.WON = 'won';
-exports.NOT_OPENED = 'not_opened';
-exports.OPENED = 'opened';
